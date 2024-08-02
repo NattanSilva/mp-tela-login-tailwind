f2d0ea46363c38c12c6edb9171c508afeb279b1a
NattanSilva <natanxboy@gmail.com>
2024-08-02 01:36:42 -0300

    add: initial configurations

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-r from-black to-gray-950">
+      <h1 className="text-5xl text-gray-200 font-bold animate-bounce">
+        Olá Mundo
+      </h1>
+      <h1 className="text-5xl font-bold rainbow-gradient animate-bounce">!</h1>
+    </div>
+  );
+}
+
+export default App;
